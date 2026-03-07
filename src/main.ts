648737c92336dd282883d4149a5f5851839e58b4
brady Fresnel <mokumabrady13@gmail.com>
2026-03-07 15:01:56 +0100

    configuration du main

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv';
 
 // Charger les variables d'environnement
 dotenv.config();
-
+/*
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
@@ -17,5 +17,26 @@ async function bootstrap() {
   const port = process.env.PORT || 5000;
   await app.listen(port, '0.0.0.0');
   console.log(`L'application est lancée sur le port: ${port}`);
+}
+bootstrap();
+*/
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'http://localhost:3000',
+      'mon_lien_frontend_sur_vercel',
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS', 'PUT'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  const port = process.env.PORT || 5000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
